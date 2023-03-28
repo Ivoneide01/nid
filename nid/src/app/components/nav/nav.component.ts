26094bc1111a15733700cf024eb4d5e3a571b081
BrunoWeezyy <neidinidnid@hotmail.com>
2023-03-28 11:52:59 -0300

    angular material

---
+++ nid/src/app/components/nav/nav.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  template: `
+    <p>
+      nav works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class NavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
